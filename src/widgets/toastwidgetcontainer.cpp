3c8eb2c0f0b298f6de4197fbaf3325cb62dd359c
Stephen Punak <steve@punak.com>
2024-10-08 11:26:02 -0700

    mid-work

--- src/widgets/toastwidgetcontainer.cpp
+++ src/widgets/toastwidgetcontainer.cpp
@@ -2,8 +2,12 @@
 
 #include <Kanoop/gui/utility/stylesheet.h>
 
+#include <QGraphicsOpacityEffect>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QPainter>
+#include <QResizeEvent>
+#include <QTimer>
 #include <QToolButton>
 
 #include <Kanoop/geometry/point.h>
@@ -14,16 +18,18 @@
 namespace Colors = QColorConstants::Svg;
 
 ToastWidgetContainer::ToastWidgetContainer(QWidget *parent) :
-    QFrame(parent)
+    QFrame(parent),
+    _autoCloseTime(TimeSpan::fromSeconds(5))
 {
+    // setAttribute(Qt::WA_TranslucentBackground);
+    // setWindowFlags(Qt::FramelessWindowHint);
     setAttribute(Qt::WA_TransparentForMouseEvents);
-    // setStyleSheet(StyleSheets::backgroundColor(Qt::blue));
-    // setStyleSheet(StyleSheets::borderRadius(10));
 }
 
 void ToastWidgetContainer::displayToast(const QString& text, const QColor& color)
 {
-    ToastWidget* toast = new ToastWidget(text, color, this);
+    ToastWidget* toast = new ToastWidget(text, color, _autoCloseTime, this);
+    connect(toast, &ToastWidget::complete, this, &ToastWidgetContainer::closeToast);
 
     QFontMetrics fm(font());
     QRect boundingRect = fm.boundingRect(0, 0, width(), 0, Qt::TextWordWrap, text);
@@ -51,20 +57,50 @@ void ToastWidgetContainer::performLayout()
     }
 }
 
-ToastWidget::ToastWidget(const QString& text, const QColor& color, QWidget* parent) :
-    QFrame(parent),
-    _createTime(QDateTime::currentDateTimeUtc())
+void ToastWidgetContainer::closeToast()
 {
+    Log::logText(LVL_DEBUG, "Close toast");
+    ToastWidget* widget = dynamic_cast<ToastWidget*>(sender());
+    if(widget != nullptr) {
+        delete widget;
+        performLayout();
+    }
+}
+
+ToastWidget::ToastWidget(const QString& text, const QColor& color, const TimeSpan& closeTime, QWidget* parent) :
+    QFrame(parent),
+    _color(color),
+    _createTime(QDateTime::currentDateTimeUtc()),
+    _opacity(1.0)
+{
+    // setAttribute(Qt::WA_TranslucentBackground);
+    // setWindowFlags(Qt::FramelessWindowHint);
+    setAttribute(Qt::WA_TransparentForMouseEvents, false);
+
     _closeButton = new QToolButton(this);
     _closeButton->setIcon(Resources::getIcon(Resources::CloseButton));
     _closeButton->setFixedSize(16, 16);
+    _closeButton->setAttribute(Qt::WA_TransparentForMouseEvents, false);
+    connect(_closeButton, &QToolButton::clicked, this, &ToastWidget::complete);
+    connect(_closeButton, &QToolButton::clicked, this, &ToastWidget::onClick);
 
     _label = new Label(text, this);
     _label->setWordWrap(true);
     _label->setAlignment(Qt::AlignCenter);
     _label->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::MinimumExpanding);
-    _label->setBackgroundColor(Colors::lightcoral);
 
+    _opacityEffect = new QGraphicsOpacityEffect(this);
+    _opacityEffect->setOpacity(_opacity);
+    setGraphicsEffect(_opacityEffect);
+
+    StyleSheet<QFrame> ss;
+    ss.setProperty(SP_Background, color);
+    ss.setPropertyPixels(SP_BorderRadius, 10);
+    setStyleSheet(ss.toString());
+
+    Q_UNUSED(closeTime)
+    // QTimer::singleShot(closeTime.totalMilliseconds(), this, &ToastWidget::onCloseTimer);
+#if 0
     QVBoxLayout* layout = new QVBoxLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
@@ -80,6 +116,54 @@ ToastWidget::ToastWidget(const QString& text, const QColor& color, QWidget* pare
     StyleSheet<QFrame> ss;
     ss.setProperty(SP_Background, color);
     ss.setPropertyPixels(SP_BorderRadius, 10);
-
     setStyleSheet(ss.toString());
+#endif
+}
+
+void ToastWidget::resizeEvent(QResizeEvent* event)
+{
+    QFrame::resizeEvent(event);
+    _label->resize(event->size());
+    QPoint pos(event->size().width() - _closeButton->width(), 0);
+    _closeButton->move(pos);
+    _closeButton->raise();
+}
+
+void ToastWidget::paintEvent(QPaintEvent* event)
+{
+    QFrame::paintEvent(event);
+    Q_UNUSED(event)
+
+#if 0
+    QPainter painter(this);
+    painter.setBrush(_color);
+    painter.setOpacity(_opacity);
+    painter.fillRect(rect(), _color);
+#endif
+}
+
+void ToastWidget::mousePressEvent(QMouseEvent* event)
+{
+    Log::logText(LVL_DEBUG, "Mouse press");
+    QFrame::mousePressEvent(event);
+}
+
+void ToastWidget::onCloseTimer()
+{
+    _opacity -= .02;
+    if(_opacity <= 0) {
+        emit complete();
+    }
+    else {
+        _opacityEffect->setOpacity(_opacity);
+
+        // setWindowOpacity(_opacity);
+        update();
+        QTimer::singleShot(20, this, &ToastWidget::onCloseTimer);
+    }
+}
+
+void ToastWidget::onClick()
+{
+    Log::logText(LVL_DEBUG, "Clicked");
 }
--- src/widgets/toastwidgetcontainer.h
+++ src/widgets/toastwidgetcontainer.h
@@ -4,6 +4,9 @@
 #include <QDateTime>
 #include <QFrame>
 
+#include <Kanoop/timespan.h>
+
+class QGraphicsOpacityEffect;
 class Label;
 class QToolButton;
 class QLabel;
@@ -14,25 +17,46 @@ public:
     explicit ToastWidgetContainer(QWidget *parent = nullptr);
     void displayToast(const QString& text, const QColor& color);
 
+    TimeSpan autoCloseTime() const { return _autoCloseTime; }
+    void setAutoCloseTime(const TimeSpan& value) { _autoCloseTime = value; }
+
 private:
     void performLayout();
 
+    TimeSpan _autoCloseTime;
+
 signals:
 
+private slots:
+    void closeToast();
 };
 
 class ToastWidget : public QFrame
 {
     Q_OBJECT
 public:
-    ToastWidget(const QString& text, const QColor& color, QWidget* parent = nullptr);
+    ToastWidget(const QString& text, const QColor& color, const TimeSpan& closeTime, QWidget* parent = nullptr);
 
     QDateTime createTime() const { return _createTime; }
 
 private:
+    virtual void resizeEvent(QResizeEvent* event) override;
+    virtual void paintEvent(QPaintEvent* event) override;
+    virtual void mousePressEvent(QMouseEvent* event) override;
+
     Label* _label;
     QToolButton* _closeButton;
+    QColor _color;
     QDateTime _createTime;
+    QGraphicsOpacityEffect* _opacityEffect = nullptr;
+    double _opacity = 1.0;
+
+signals:
+    void complete();
+
+private slots:
+    void onCloseTimer();
+    void onClick();
 };
 
 #endif // TOASTWIDGETCONTAINER_H
